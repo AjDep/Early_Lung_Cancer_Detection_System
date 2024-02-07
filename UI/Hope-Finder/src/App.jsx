56b74a608e3a5f0139c49c389e6f40950a96cc60
Bhathiya Sahan <bhathiyasahanedu@gmail.com>
2024-02-08 00:39:21 +0530

    Create React projec
    Create a react project and design template

---
+++ UI/Hope-Finder/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='navbar'></div>
+      <div className='home'>
+        <div className='header'>
+          <h1>Analysis</h1>
+        </div>
+        <div className='content'>
+          <h1>i</h1>
+        </div>
+      </div>
+    </div>
+      
+  );
+}
+
+export default App
