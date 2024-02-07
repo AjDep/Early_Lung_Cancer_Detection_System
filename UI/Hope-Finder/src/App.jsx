ecf57e5eadbdf6f64c542dbc74e3eea254bc206b
Bhathiya Sahan <bhathiyasahanedu@gmail.com>
2024-02-08 04:40:46 +0530

    Declare dashboard
    Declare dashboard page and add navbar to it

--- UI/Hope-Finder/src/App.jsx
+++ UI/Hope-Finder/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Analysis from './Pages/Analysis/analysis';
+import Dashboard from './Pages/Dashboard/dashnoard';
 
 function App() {
   return (
     <div className="App">
-      <Analysis/>
+      {/*<Analysis/>
+      <Dashboard/>*/}
+      <Dashboard/>
     </div>
       
   );
