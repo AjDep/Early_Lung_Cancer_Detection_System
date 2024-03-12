4067847c660bbd41e50d84a86d98d5de65e77d8e
Bhathiya <120437641+Bhathiya-sahan@users.noreply.github.com>
2024-03-12 06:53:40 +0000

    Delete some files

--- Backend/middleware/errorMiddleware.js
+++
@@ -1,6 +0,0 @@
-// Middleware functions written in Express related to error handling
-
-exports.errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Internal Server Error');
-  };
