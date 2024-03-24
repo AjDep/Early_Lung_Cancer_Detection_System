34d15c45fa46933ce28de2e7405d0fb77e56aa75
AjDep <w.anjanadep@gmail.com>
2024-03-25 02:29:32 +0530

    updated

--- Client/src/components/Analysis/details.jsx
+++ Client/src/components/Analysis/details.jsx
@@ -5,11 +5,8 @@ import Col from "react-bootstrap/Col";
 function Details(props) {
   return (
     <div className={style.card}>
-      <div>
-        <h6>{props.title}</h6>
-      </div>
-      
       <div id={style.box} className="p-2">
+      <h6 className={style.Titlebox}>{props.title}</h6>
         <p className={style.text}>{props.value}</p>
       </div>
     </div>
