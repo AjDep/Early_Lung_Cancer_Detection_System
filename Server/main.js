50007f2fbfefe072bbac36c2d37b8f6f78b4183d
AjDep <w.anjanadep@gmail.com>
2024-04-04 11:07:09 +0530

    updateOndevice

--- Server/main.js
+++ Server/main.js
@@ -3,7 +3,7 @@ const { ReadlineParser } = require('@serialport/parser-readline');
 const port = new SerialPort({ path: 'COM5', baudRate: 9600 });
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
-
+let Data = null;
 port.on('error', (err) => {
     console.error('Device not connected');
     if(err.message){
@@ -18,10 +18,15 @@ parser.on('error', (err) => {
 });
 
 parser.on('data', console.log);
+parser.on('data', catchData);
+
+function catchData(data){
+    Data = data;
+}
 
 function onData() {
     const data = null;
-    return data;
+    return Data;
 }
 
 module.exports = onData;
